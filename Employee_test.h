185a2681e362fc6e5c49efe3f8a37e8497ac1f6c
xiao <72422851+XiaoFan-UCR-CompEngr@users.noreply.github.com>
2020-12-09 13:32:52 -0800

    push

---
+++ Employee_test.h
@@ -0,0 +1,13 @@
+//
+// Created by 范笑 on 12/9/20.
+//
+
+#ifndef TESTINGBUGS_EMPLOYEE_TEST_H
+#define TESTINGBUGS_EMPLOYEE_TEST_H
+
+#include <gtest/gtest.h>
+TEST(CEOTEST, inputTest) {
+
+EXPECT_EQ(1,1);
+}
+#endif //TESTINGBUGS_EMPLOYEE_TEST_H
---
+++ unit_test.cpp
@@ -0,0 +1,12 @@
+//
+// Created by 范笑 on 12/9/20.
+//
+//
+// Created by 范笑 on 12/8/20.
+//
+#include "gtest/gtest.h"
+#include "Employee_test.h"
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
